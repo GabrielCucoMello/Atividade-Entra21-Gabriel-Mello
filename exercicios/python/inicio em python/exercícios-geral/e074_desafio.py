0e2a301ee71ba2b7cf6e8f340a5e16117313b0d3
Gabriel Cuco Mello <40546046+Gabreelzo@users.noreply.github.com>
2022-06-07 21:41:46 -0300

    adicionado desafio do exercício e074

---
+++ exercicios/python/inicio em python/exercícios-geral/e074_desafio.py
@@ -0,0 +1,33 @@
+class Calculadora:
+    result = 0
+
+    def __init__(self, op, num1, num2):
+        self.num1 = num1
+        self.num2 = num2
+        self.op = op
+        _f = str(num1) + str(self.op) + str(num2)
+        self.result = eval(_f)
+    
+    #Getter
+    @property
+    def op(self):
+        return self.op_valido
+    
+    #Setter
+    @op.setter
+    def op(self, op):
+        if isinstance(op, str):
+            op = str(op.replace('x', '*'))
+        if isinstance(op, str):
+            op = str(op.replace('.', '*'))
+        if isinstance(op, str):
+            op = str(op.replace('^', '**'))
+            
+        self.op_valido = op
+
+num1 = int(input('Digite um primeiro número para ser calculado: '))
+op = input('Informe o operador: ')
+num2 = int(input('Digite um segundo número para ser calculado: '))
+
+resultado = Calculadora(op, num1, num2)
+print(resultado.result)
