906876eaa6ba62dc295bb9346e5d6a23db0f6c85
Gabriel Cuco Mello <40546046+Gabreelzo@users.noreply.github.com>
2022-06-27 19:24:41 -0300

    adicionado ultimos exercicios

--- exercicios/python/inicio_em_python/exercicios-geral/e083.py
+++ exercicios/python/inicio_em_python/exercicios-geral/e083.py
@@ -1,6 +1,13 @@
+import math
+
+
 class Romano():
-    def __init__(self):
-        self.romanDict =  {
+    def __init__(self, romanDict):
+        self.romanDict = romanDict
+
+    @classmethod
+    def inteiroRomano(cls, A):
+        cls.romanDict = {
             1: "I",
             5: "V",
             10: "X",
@@ -12,4 +19,31 @@ class Romano():
             10000: "H"
         }
 
-        # https://acervolima.com/programa-python-para-converter-inteiros-em-romanos/
+        div = 1
+        while A >= div:
+            div *= 10
+        
+        div /= 10
+
+        resultado = ""
+
+        while A:
+            lastNum = int(A / div)
+
+            if lastNum <= 3:
+                resultado += (cls.romanDict[div] * lastNum)
+            elif lastNum == 4:
+                resultado += (cls.romanDict[div] + cls.romanDict[div * 5])
+            elif 5 <= lastNum <= 8:
+                resultado += (cls.romanDict[div * 5] + (cls.romanDict[div] * (lastNum - 5)))
+            elif lastNum == 9:
+                resultado += (cls.romanDict[div] + cls.romanDict[div * 10])
+            
+            A = math.floor(A % div)
+            div /= 10
+        
+        return resultado
+
+valor = int(input('Digite um valor: '))
+saida = Romano.inteiroRomano(valor)
+print(saida)
--- exercicios/python/inicio_em_python/exercicios-geral/e099.py
+++ exercicios/python/inicio_em_python/exercicios-geral/e099.py
@@ -6,4 +6,19 @@ class Carros():
         self.cor = cor
         self.valor = valor
 
-# Tem q terminar, mas to com sono demais pra isso
+    def descricao(self):
+        descricao_carro = f'Marca: {self.marca}, Modelo: {self.modelo}, Cor: {self.cor}, Ano: {self.ano}, Valor: {self.valor}.'
+        return descricao_carro
+
+
+carro1 = Carros('Toyota', 'Supra', '1993', 'Renaissance Red', 'R$ 1.000.000')
+carro2 = Carros('Nissan', 'Skyline R34 GTR', '1999', 'Midnight Purple 3', 'R$ 1.652.000')
+carro3 = Carros('Chevrolet', 'Opala SS', '1974', 'Vermelho', 'R$ 250.000')
+
+print(carro1)
+print(carro2)
+print(carro3)
+
+print(carro1.descricao())
+print(carro2.descricao())
+print(carro3.descricao())
